42570e8d4e944a26ce12f7cf04cbd9c5ab84c5e6
Ali Shabani <pyramidalish@gmail.com>
2017-12-31 12:39:05 +0330

    added sentry

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Fedeisas\LaravelMailCssInliner\LaravelMailCssInlinerServiceProvider::class,
-        Alish\GoogleDistanceMatrix\GoogleDistanceServiceProvider::class
+        Alish\GoogleDistanceMatrix\GoogleDistanceServiceProvider::class,
+        Sentry\SentryLaravel\SentryLaravelServiceProvider::class
     ],
 
     /*
@@ -226,8 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GoogleDistance' => Alish\GoogleDistanceMatrix\Facades\GoogleDistance::class
-
+        'GoogleDistance' => Alish\GoogleDistanceMatrix\Facades\GoogleDistance::class,
+        'Sentry' => Sentry\SentryLaravel\SentryFacade::class
     ],
 
 ];
