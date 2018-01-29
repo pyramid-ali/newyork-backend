07d8933cbb3d68a1ff9f2a3beeaa165f67da5056
Ali Shabani <pyramidalish@gmail.com>
2018-01-29 23:23:36 +0330

    change work time calculation

--- app/Ny/Services/WorkTime.php
+++ app/Ny/Services/WorkTime.php
@@ -4,12 +4,20 @@ namespace App\Ny\Services;
 
 
 use App\Employee;
+use Carbon\Carbon;
 
 class WorkTime implements ServiceWorker
 {
 
     public function work($job, Employee $employee)
     {
-        return ['reg_hours' => $employee->tehd];
+
+        $startTime = Carbon::parse($job['start_datetime']);
+        $endTime = Carbon::parse($job['end_datetime']);
+        $minutes = $endTime->diffInMinutes($startTime);
+        $hours = $endTime->diffInHours($startTime);
+        $exactHour = $hours + ($minutes - ($hours * 60)) / 60;
+
+        return ['reg_hours' => $exactHour * 0.75];
     }
 }
