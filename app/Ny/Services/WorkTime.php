2a70d0ecc5d1d16bd7e850a2caa477e3292c8c1f
Ali Shabani <pyramidalish@gmail.com>
2018-03-29 03:45:15 +0430

    change work time of ft_office to be same as pdm

--- app/Ny/Services/WorkTime.php
+++ app/Ny/Services/WorkTime.php
@@ -12,19 +12,15 @@ class WorkTime implements ServiceWorker
     public function work($job, Employee $employee)
     {
 
-        if ($employee->employee_type === 'ft_office') {
-            return ['reg_hours' => 8];
-        }
-
         $startTime = Carbon::parse($job['start_datetime']);
         $endTime = Carbon::parse($job['end_datetime']);
         $minutes = $endTime->diffInMinutes($startTime);
         $hours = $endTime->diffInHours($startTime);
         $exactHour = $hours + ($minutes - ($hours * 60)) / 60;
 
-        $multiplier = 0.75;
-        if ($employee->employee_type === 'pdm') {
-            $multiplier = 1;
+        $multiplier = 1;
+        if ($employee->employee_type === 'ft_patient') {
+            $multiplier = 0.75;
         }
 
         return ['reg_hours' => $exactHour * $multiplier];
